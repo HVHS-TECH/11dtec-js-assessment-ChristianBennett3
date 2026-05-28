307b18a69bd9f09b81dd0e5eb52b9a41698a8bc6
ChristianBennett3 <24204cb@hvhs.school.nz>
2026-05-28 13:28:31 +1200

    Starting DIVS

--- script.js
+++ script.js
@@ -1,11 +1,2 @@
 var pies = ["minceCheese", "mince", "steakCheese", "steak", "butterChicken", "baconEgg", "potatoTop"]
-var orderInfo;
 
-
-function getFormInput(){
-    const PIE_NAME_FIELD = document.getElementById("pieNameField");
-    orderInfo = {
-        userOrder: PIE_NAME_FIELD.value
-    }
-    console.log(orderInfo.userOrder)
-}
