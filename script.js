a32a0a1f73e748822726ed77c4018f542cd349d9
ChristianBennett3 <24204cb@hvhs.school.nz>
2026-06-26 15:09:49 +1200

    Forms and Objects in progress

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ Variables
  *************************************/
 
 var pies = ["Mince and Cheese", "Steak and Cheese", "Butter Chicken", "Apple"];
+var orderInfo;
 const OrderOutput = document.getElementById("PieOrderOutput");
 const ItemsOutput = document.getElementById("ItemsOutput");
 
@@ -23,14 +24,25 @@ Functions
 function addPie(_type, _price){
     console.log(_type, _price); 
     ItemsOutput.innerHTML += "<p>" + pies[_type] + ": $" + _price + "</p>";
+    
 }
 
 function getFormInput(){
     const NAME_FIELD = document.getElementById("nameField");
     const MONEY_FIELD = document.getElementById("moneyField");
-    userInfo = {
+    orderInfo = {
         userName: NAME_FIELD.value,
         userMoney: Number(MONEY_FIELD.value),
     }
-    OrderOutput.innerHTML += "<p>" + userInfo.userName + "<br>" + userInfo.userMoney + "</p>"
+
+    if (typeof orderInfo.userName != "string" || orderInfo.userName == "" || orderInfo.userName.length < 3){
+        alert("Please enter a valid name")
+        return
+    }
+    start()
 }
+
+function start(){
+    OrderOutput.innerHTML += "<p>Name: " + orderInfo.userName + "<br>Money: " + orderInfo.userMoney + "</p>"
+}
+
